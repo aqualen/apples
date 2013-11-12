08c11514de87e56ec1d219485dd9d037e5d0a096
Leonard Vaughn <lvaughn@climate.com>
2013-11-12 05:15:31 -0800

    Move business logic into service

--- app/grails-app/controllers/com/ljs/apples/QuoteController.groovy
+++ app/grails-app/controllers/com/ljs/apples/QuoteController.groovy
@@ -4,40 +4,10 @@ class QuoteController {
 
   static scaffold = true
 
-  //  static {
-  //    withoutFeature('quoteScaffold') {
-  //      scaffold = false
-  //    }
-  //  }
+  def quoteService
 
-  def index() {
-    //    withFeature('quote') {
-    //      redirect(action: "home")
-    //    }
-  }
-
-  def home() {
-    // http://localhost:8090/app/quote/home
-    withFeature('quote') {
-      render "<h1>Some day, something interesting will be here!</h1>"
-    }
-    withoutFeature('quote') {
-      render "<h1>Stay tuned, good stuff coming soon!</h1>"
-    }
-  }
-
-  def random() {
-    withFeature('randomQuote') {
-      def allQuotes = Quote.list()
-      def randomQuote
-      if (allQuotes.size() > 0) {
-        def randomIdx = new Random().nextInt(allQuotes.size())
-        randomQuote = allQuotes[randomIdx]
-      } else {
-        randomQuote = new Quote(author: "Anonymous",
-                                content: "Real Programmers Don't eat Quiche")
-      }
-      [quote: randomQuote]
-    }
+  def random = {
+    def randomQuote = quoteService.getRandomQuote()
+    [quote: randomQuote]
   }
 }
